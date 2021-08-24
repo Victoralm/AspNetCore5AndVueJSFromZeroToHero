8b83be9356bc251af2bb9b7467c1c5e18ed6f774
victoralm <victoralmlinx@gmail.com>
2021-08-24 20:18:22 -0300

    Sec01-Les22: How to create a Connection Service

---
+++ Adonet-Blog/Services/ConnectionService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adonet_Blog.Services
+{
+    public class ConnectionService
+    {
+        private SqlConnection _conn;
+        private IConfiguration _config;
+
+        public ConnectionService(IConfiguration config)
+        {
+            this._config = config;
+            string connectionString = _config.GetConnectionString("Default").ToString();
+            this._conn = new SqlConnection(connectionString);
+
+            // Opens the connection with the Db if not opened already
+            if (this._conn.State != ConnectionState.Open)
+            {
+                this._conn.Open();
+            }
+        }
+
+        internal SqlConnection DbConnection()
+        {
+            return this._conn;
+        }
+    }
+}
---
+++ Adonet-Blog/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adonet_Blog.Services
+{
+    /// <summary>
+    /// Used to get and send data to the User Db table
+    /// </summary>
+    public class UserService
+    {
+        private SqlConnection _conn;
+        SqlCommand _command;
+
+        
+        public UserService(IConfiguration config)
+        {
+            ConnectionService connServ = new ConnectionService(config);
+            this._conn = connServ.DbConnection();
+        }
+    }
+}
