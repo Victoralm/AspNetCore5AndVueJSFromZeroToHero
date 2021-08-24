25d637cbe932e98dbe77297a9bc0f1036005d4bc
victoralm <victoralmlinx@gmail.com>
2021-08-24 12:08:39 -0300

    Sec01-Les12: How to create Db Entities Creating POCO classes for the tables of the Db

---
+++ Adonet-Blog/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adonet_Blog.Entities
+{
+    /// <summary>
+    /// POCO class for the User DB table
+    /// </summary>
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
