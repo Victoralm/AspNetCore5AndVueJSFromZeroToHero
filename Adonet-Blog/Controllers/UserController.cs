9a8f0ef1f7094e3fe8d80cddf2f968de144b2c52
victoralm <victoralmlinx@gmail.com>
2021-08-24 22:47:26 -0300

    Sec01-Les26: How to create a user login Action Result

--- Adonet-Blog/Controllers/UserController.cs
+++ Adonet-Blog/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Adonet_Blog.Entities;
+using Adonet_Blog.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +15,19 @@ namespace Adonet_Blog.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            BlogModel model = new BlogModel
+            {
+                user = new User(),
+            };
             return View();
         }
     }
