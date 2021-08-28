7c878cd24829864a2bcc6304f69187e40a91e0dc
victoralm <victoralmlinx@gmail.com>
2021-08-27 21:49:23 -0300

    Sec03-Les78: How to display Categories in our Template file

--- DapperFantom/Startup.cs
+++ DapperFantom/Startup.cs
@@ -1,3 +1,4 @@
+using DapperFantom.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace DapperFantom
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddScoped<CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
