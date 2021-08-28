e4d8240237a7fc2a657ab454f34250b95f4ab1c3
victoralm <victoralmlinx@gmail.com>
2021-08-27 23:38:15 -0300

    Sec03-Les81: How to display Categories in our Template file

--- DapperFantom/Controllers/ArticleController.cs
+++ DapperFantom/Controllers/ArticleController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DapperFantom.Entities;
+using DapperFantom.Models;
+using DapperFantom.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +12,16 @@ namespace DapperFantom.Controllers
 {
     public class ArticleController : Controller
     {
+        private CategoryService _categoryService;
+
+        public object GeneralModel { get; private set; }
+
+        public ArticleController(IServiceProvider serviceProvider)
+        {
+            // Does de correct dependency injection of the desired service class
+            // An alternative to injecting as a parameter on the constructor
+            this._categoryService = serviceProvider.GetRequiredService<CategoryService>();
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,7 +29,14 @@ namespace DapperFantom.Controllers
         
         public IActionResult Add()
         {
-            return View();
+            List<Category> categList = this._categoryService.GetAllCategDapper();
+
+            GeneralViewModel model = new GeneralViewModel
+            {
+                CategorieList = categList,
+            };
+
+            return View(model);
         }
     }
 }
