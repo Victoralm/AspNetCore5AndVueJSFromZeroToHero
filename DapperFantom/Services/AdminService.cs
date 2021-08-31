0f069fc1ba229ea1af7b0991d3fab1b4538642e4
victoralm <victoralmlinx@gmail.com>
2021-08-31 02:01:00 -0300

    Sec03-Les96: How to create Admin Service

---
+++ DapperFantom/Services/AdminService.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using DapperFantom.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperFantom.Services
+{
+    public class AdminService
+    {
+        private IDbConnection _dapperConnection;
+        private ConnectionService _connectionService;
+
+        public AdminService(IConfiguration config)
+        {
+            this._connectionService = new ConnectionService(config);
+            this._dapperConnection = this._connectionService.ForDapper();
+        }
+
+        public Admin Login(Admin adm)
+        {
+            Admin admin = new Admin();
+
+            try
+            {
+                var par = new DynamicParameters();
+                par.Add("@Username", adm.Username);
+                par.Add("@Password", adm.Password);
+                admin = this._dapperConnection.Query<Admin>($@"select id as [AdminId],[Username],[Password] from [Admins] where [Username]=@Username and [Password]=@Password", par).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return admin;
+        }
+    }
+}
