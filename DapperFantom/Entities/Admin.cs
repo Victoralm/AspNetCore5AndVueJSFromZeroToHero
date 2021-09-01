49605c065f00ecffbb369b704e48f921de3f89dc
victoralm <victoralmlinx@gmail.com>
2021-09-01 16:11:26 -0300

    Sec03-Les99: How to use data annotations for the Login Form

--- DapperFantom/Entities/Admin.cs
+++ DapperFantom/Entities/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,19 @@ namespace DapperFantom.Entities
     /// </summary>
     public class Admin
     {
+        [Dapper.Contrib.Extensions.Key]
         public int AdminId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your username")]
+        [MinLength(4, ErrorMessage = "Min 4 characters")]
+        [MaxLength(50, ErrorMessage = "Max 50 characters")]
+        [Display(Name = "Your Username")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [MinLength(4, ErrorMessage = "Min 4 characters")]
+        [MaxLength(50, ErrorMessage = "Max 50 characters")]
+        [Display(Name = "Your Password")]
         public string Password { get; set; }
     }
 }
