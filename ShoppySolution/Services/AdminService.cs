051f9df42a16f3af0aeefe6b1bc1535c7585bb52
victoralm <victoralmlinx@gmail.com>
2021-09-13 07:48:46 -0300

    S04-L171: How to create admin Service functions

--- ShoppySolution/Services/AdminService.cs
+++ ShoppySolution/Services/AdminService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,5 +45,56 @@ namespace Services
             }
         }
 
+
+        public void Update(Admin entity)
+        {
+            using (var context = new DatabaseContext())
+            {
+                var updateAdmin = context.Entry(entity);
+                updateAdmin.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Does the same as Get
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Admin GetById(int id)
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Set<Admin>().Where(x => x.Id == id).FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// Does the same as GetById
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public Admin Get(Expression<Func<Admin, bool>> predicate = null)
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Set<Admin>()
+                    // If return null, throw an exception
+                    .FirstOrDefault(predicate ?? throw new ArgumentException(nameof(predicate)));
+            }
+        }
+
+        public List<Admin> GetList(Expression<Func<Admin, bool>> filter = null)
+        {
+            using (var context = new DatabaseContext())
+            {
+                // If filter is null
+                return filter == null
+                    // return a list of all Admin records
+                    ? context.Set<Admin>().ToList()
+                    // else, return a list of Admin records based on the filter
+                    : context.Set<Admin>().Where(filter).ToList();
+            }
+        }
     }
 }
---
+++ ShoppySolution/Services/README.md
@@ -0,0 +1,4 @@
+# Services
+
+Also known as Application layer, does the mediation between the App ([Shoppy](../Shoppy/))
+and the Persistence ([DAL](../DAL/)) layers
